7e131c6913db7d4fdd3096f963355fc0c48a9357
0xbigz <83473873+0xbigz@users.noreply.github.com>
2023-10-22 21:39:39 -0400

    cleanup data dumps

--- jup_snap.py
+++ jup_snap.py
@@ -2,6 +2,16 @@ import requests
 import pandas as pd
 import time
 
+est_prices = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=usd-coin,wrapped-solana&vs_currencies=usd').json()
+
+sol_price = est_prices['wrapped-solana']['usd']
+
+precision_map = {
+    'So11111111111111111111111111111111111111112': {'precision': 9, 'name': 'SOL'},
+    'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v': {'precision': 6, 'name': 'USDC'},
+
+}
+
 # List of mint pairs
 mints = [
     ('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v', 'So11111111111111111111111111111111111111112'),
@@ -9,18 +19,26 @@ mints = [
     # Add more mint pairs as needed
 ]
 
-amounts = [5000, 10000, 50000, 100000]
+usdc_amounts = [5_000, 10_000, 50_000, 100_000]
 
 for inputMint, outputMint in mints:
     now = time.time()
     result = {}
 
-    for amount in amounts:
+    for i, usdc_amount in enumerate(usdc_amounts):
+        inAmount = 0
+        if inputMint == 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v':
+            inAmount = int(usdc_amount * (10 ** precision_map[inputMint]['precision']))
+        else:
+            est_price = sol_price
+            inAmount = int(usdc_amount * (10 ** precision_map[inputMint]['precision'] / est_price))
         url = f'https://quote-api.jup.ag/v6/quote?inputMint={inputMint}&outputMint={outputMint}'
-        url += f'&amount={amount}&slippageBps=10&onlyDirectRoutes=false&asLegacyTransaction=false'
+        url += f'&amount={inAmount}&slippageBps=10&onlyDirectRoutes=false&asLegacyTransaction=false'
 
         try:
-            result[amount] = requests.get(url).json()
+            jj = requests.get(url).json()
+            if 'error' not in list(jj.keys()): 
+                result[i] = jj
         except:
             pass
 
@@ -32,7 +50,7 @@ for inputMint, outputMint in mints:
     # todo here
     mint_symbols = ['USDC' if mint_symbol != 'So11111111111111111111111111111111111111112' else 'SOL' for mint_symbol in mint_symbols]
     # Generate the filename
-    filename = f"data/jup/{mint_symbols[0]}-{mint_symbols[1]}/{int(now)}-{int(latency)}.csv"
+    filename = f"data/jup/{mint_symbols[1]}-{mint_symbols[0]}/{int(now)}-{int(latency)}.csv"
 
     df = pd.DataFrame(result)
     df.to_csv(filename)
